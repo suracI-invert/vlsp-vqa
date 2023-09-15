07b5f5ed7b36d87da89754b08b58b4a0c2872ee0
AESir2412 <21020512@vnu.edu.vn>
2023-09-15 20:31:42 +0700

    Update attentions (for text)

--- src/model/components/language/encoders.py
+++ src/model/components/language/encoders.py
@@ -9,18 +9,20 @@ class ViT5Encoder(Module):
     """
     def __init__(
         self,
-        pretrained = 'VietAI/vit5-large' # Can change to large or base
+        pretrained = 'VietAI/vit5-large', # Can change to large or base
+        max_length = 128,
     ) -> None:
         super().__init__()
         self.model = AutoModel.from_pretrained(pretrained)
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained)
+        self.max_length = max_length
 
     def forward(self, text):
         """
             - input: text
-            - output shape: (batch_size, sequence_length, hidden_size) [1, ?, 768]
+            - output shape: (batch_size, sequence_length, hidden_size) [1, max_length, 768]
         """
-        vit5_encoding = self.tokenizer(text, return_tensors = "pt", padding = 'max_length') 
+        vit5_encoding = self.tokenizer(text, return_tensors = "pt", padding = 'max_length', max_length= self.max_length) 
         # Chưa biết có cần thêm max_length = 1024 khum, nếu thêm thì seq_len = 1024
         vit5_input_ids, vit5_attention_masks = vit5_encoding["input_ids"], vit5_encoding["attention_mask"]
         outputs = self.model.encoder(
@@ -39,17 +41,19 @@ class BARTphoEncoder(Module):
     """
     def __init__(
         self,
-        pretrained = 'vinai/bartpho-syllable' # Can change to word or syllable
+        pretrained = 'vinai/bartpho-syllable', # Can change to word or syllable
+        max_length = 128,
     ) -> None:
         super().__init__()
         self.model = AutoModel.from_pretrained(pretrained)
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained)
+        self.max_length = max_length
     
     def forward(self, text):
         """
         - input: text
         - output shape: (batch_size, sequence_length, hidden_size) [1, 1024, 1024]
         """
-        bartpho_input_ids = self.tokenizer(text, return_tensors = "pt", padding = 'max_length')
+        bartpho_input_ids = self.tokenizer(text, return_tensors = "pt", padding = 'max_length', max_length= self.max_length)
         outputs = self.model(**bartpho_input_ids)
         return outputs.last_hidden_state

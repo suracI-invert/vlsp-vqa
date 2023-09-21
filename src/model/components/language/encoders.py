14b54e0dbbeac198ded276770192cc06191e401b
AESir2412 <21020512@vnu.edu.vn>
2023-09-21 15:02:10 +0700

    fixing encoder (add hidden size)

--- src/model/components/language/encoders.py
+++ src/model/components/language/encoders.py
@@ -1,4 +1,5 @@
 """import modules"""
+from torch import nn
 from torch.nn import Module
 from transformers import AutoModel, AutoTokenizer, AutoModelForSeq2SeqLM
 
@@ -10,14 +11,16 @@ class ViT5Encoder(Module):
     def __init__(
         self,
         pretrained = 'VietAI/vit5-base', # Can change to large or base
+        hidden_dim = 768
     ) -> None:
         super().__init__()
         self.model = AutoModel.from_pretrained(pretrained)
+        self.hidden_dim = hidden_dim
 
     def forward(self, input):
         """
             - input: input_ids (encoding in this case)
-            - output shape: (batch_size, sequence_length, hidden_size) [1, max_length, 768]
+            - output shape: (batch_size, sequence_length, hidden_size) [1, max_length, 768] (768 for base, 1024 for large)
         """
         vit5_input_ids, vit5_attention_masks = input["input_ids"], input["attention_mask"]
         outputs = self.model.encoder(
@@ -25,7 +28,8 @@ class ViT5Encoder(Module):
             attention_mask = vit5_attention_masks,
             return_dict = True
         )
-
+        if self.hidden_dim != outputs.shape - 1:
+            outputs = nn.Linear(outputs.shape - 1, self.hidden_dim)
         return outputs.last_hidden_state
     
     def freeze(self):
@@ -41,14 +45,18 @@ class BARTphoEncoder(Module):
     def __init__(
         self,
         pretrained = 'vinai/bartpho-syllable-base', # Can change to word or syllable
+        hidden_dim = 768
     ) -> None:
         super().__init__()
         self.model = AutoModel.from_pretrained(pretrained)
-    
+        self.hidden_dim = hidden_dim
+
     def forward(self, input):
         """
         - input: input_ids
-        - output shape: (batch_size, sequence_length, hidden_size) [1, 1024, 1024]
+        - output shape: (batch_size, sequence_length, hidden_size) [1, max_length, 768] (768 for base, 1024 for large)
         """
         outputs = self.model(**input)
+        if self.hidden_dim != outputs.shape - 1:
+            outputs = nn.Linear(outputs.shape - 1, self.hidden_dim)
         return outputs.last_hidden_state

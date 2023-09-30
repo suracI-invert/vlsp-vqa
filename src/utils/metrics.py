0efe22e8f0c0913bfce35a9234ccfe9b8b836f72
suracI-invert <koolsha123@gmail.com>
2023-09-30 15:49:15 +0700

    add evaluation metric

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,29 +1,25 @@
 #TODO: Add CIDEr-D
 
 from typing import List
-from torchmetrics import Metric
-import torch
 
 from src.utils.evaluation import compute_scores
 
-class BLEU_CIDEr(Metric):
-    higher_is_better: bool = True
+class BLEU_CIDEr(object):
     def __init__(self):
-        super().__init__()
-        self.add_state('keys', default= [])
-        self.add_state('preds', default= [])
-        self.add_state('labels', default= [])
+        self.val = []
 
     def update(self, pred: List, label: List):
         for i in range(len(pred)):
-            self.preds.append(pred[i])
-            self.labels.append(label[i])
+            self.val.append((pred[i], label[i]))
     
     def compute(self):
         preds_dict = {}
         labels_dict = {}
-        for i in range(len(self.labels)):
-            preds_dict[i] = [self.preds[i]]
-            labels_dict[i] = [self.labels[i]]
+        for i in range(len(self.val)):
+            preds_dict[i] = [self.val[i][0]]
+            labels_dict[i] = [self.val[i][1]]
         score = compute_scores(labels_dict, preds_dict)
         return score
+    
+    def reset(self):
+        self.val = []
